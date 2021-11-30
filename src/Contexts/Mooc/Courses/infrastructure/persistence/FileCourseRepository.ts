74fbbae671c517c40b0bffaa9f88621221a41910
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-11-30 15:59:23 +0100

    test completos

---
+++ src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import { serialize, deserialize } from 'bson';
+import { Course } from "../../domain/Course";
+import { CourseRepository } from "../../domain/CourseRepository";
+
+export class FileCourseRepository implements CourseRepository {
+    private FILE_PATH = `${__dirname}/course`;
+    async save(note: Course): Promise<void> {
+        fs.promises.writeFile(this.filePath(note.id), serialize(note))
+    }
+
+    async search(noteId: string): Promise<Course> {
+        const courseData = await fs.promises.readFile(this.filePath(noteId));
+        const { id, name, duration } = deserialize(courseData);
+        return new Course({id, name, duration} );
+    }
+
+    private filePath(id: string): string {
+        return `${this.FILE_PATH}.${id}.repo`;
+    }
+
+}
--- src/apps/mooc/backend/routes/notes.route.ts
+++ src/apps/mooc/backend/routes/notes.route.ts
@@ -1,8 +1,8 @@
 import { Request, Response, Router } from "express";
-import { NotesPutController } from "../controllers/NotesPutController";
+import { NotePutController } from "../controllers/NotePutController";
 import container from "../dependency-injection";
 
 export const register = (router: Router)=>{
-    const controller: NotesPutController = container.get('Apps.mooc.controllers.NotesPutController');
+    const controller: NotePutController = container.get('Apps.mooc.controllers.NotePutController');
     router.put('/notes/:id', (req:Request, res: Response)=> controller.run(req,res) );
 }
